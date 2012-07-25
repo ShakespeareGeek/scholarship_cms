d4172be3648e5937cb7309bcfc44110d99600898
Duane Morin <dmorin@gmail.com>
2012-07-25 16:13:17 -0400

    do not include blank nested forms

--- app/models/scholarship_provider.rb
+++ app/models/scholarship_provider.rb
@@ -1,6 +1,6 @@
 class ScholarshipProvider < ActiveRecord::Base
   has_many :contacts, :class_name=>'ProviderContact'
   has_many :scholarships, :foreign_key=>'provider_id'
-  accepts_nested_attributes_for :scholarships
+  accepts_nested_attributes_for :scholarships, :reject_if=> proc {|attributes| attributes[:entry].blank?}, :allow_destroy => true
 
 end
